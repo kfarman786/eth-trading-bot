62946dc405c929e78291963d2d18a25d4af1b8ce
kfarman786 <furmanwebsoftrix@gmail.com>
2026-06-03 01:58:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,20 @@
-import os
 import time
 import requests
 
-BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+end_time = int(time.time())
+start_time = end_time - (86400 * 7)
 
-def send_telegram(message):
-    requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        data={
-            "chat_id": CHAT_ID,
-            "text": message
-        }
-    )
+url = "https://api.india.delta.exchange/v2/history/candles"
 
-try:
-    end_time = int(time.time())
-    start_time = end_time - (200 * 15 * 60)
+params = {
+    "symbol": "ETHUSD",
+    "resolution": "1h",
+    "start": start_time,
+    "end": end_time
+}
 
-    url = "https://api.delta.exchange/v2/history/candles"
+r = requests.get(url, params=params)
 
-    params = {
-        "symbol": "ETHUSD",
-        "resolution": "15m",
-        "start": start_time,
-        "end": end_time
-    }
-
-    r = requests.get(url, params=params)
-
-    print("STATUS:", r.status_code)
-
-    data = r.json()
-
-    print(data)
-
-    if r.status_code == 200:
-        send_telegram("✅ Delta ETHUSD candle connection working")
-    else:
-        send_telegram(f"❌ Delta API Error: {r.status_code}")
-
-except Exception as e:
-    print(e)
-    send_telegram(f"❌ Bot Error:\n{e}")
+print("STATUS:", r.status_code)
+print(r.url)
+print(r.json())
