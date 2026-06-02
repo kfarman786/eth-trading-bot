76fef16b4e63beadd2cd7737be5d4f811ba1b3a5
kfarman786 <furmanwebsoftrix@gmail.com>
2026-06-03 02:00:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,8 @@
-import time
 import requests
 
-end_time = int(time.time())
-start_time = end_time - (86400 * 7)
+url = "https://api.india.delta.exchange/v2/products/3136"
 
-url = "https://api.india.delta.exchange/v2/history/candles"
-
-params = {
-    "symbol": "ETHUSD",
-    "resolution": "1h",
-    "start": start_time,
-    "end": end_time
-}
-
-r = requests.get(url, params=params)
+r = requests.get(url)
 
 print("STATUS:", r.status_code)
-print(r.url)
-print(r.json())
+print(r.text[:1000])
