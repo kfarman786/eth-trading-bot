5ea1826e387e4ecc14e3fb8c6e419350f9a682fa
kfarman786 <furmanwebsoftrix@gmail.com>
2026-06-03 23:15:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-print("BOT WORKING")
+import os
+import requests
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+r = requests.post(
+    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": "✅ Paper Trading Bot Online"
+    }
+)
+
+print("Status:", r.status_code)
+print(r.text)
